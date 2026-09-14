bac58fbffa3fa169bc79ee38e8ca919d600d6985
Markus <ipa23.m.spulis@vtdt.edu.lv>
2026-09-14 15:16:22 +0300

    Darbs ar breeze un meiģinu to pielietot(vel nestrada)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RandomGameController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
     return view('welcome', [
@@ -12,3 +13,17 @@ Route::get('/', function (\Illuminate\Http\Request $request) {
 Route::get('/games', [RandomGameController::class, 'search'])->name('games.search');
 Route::post('/favorites/toggle', [RandomGameController::class, 'toggleFavorite'])->name('favorites.toggle');
 Route::get('/random', [RandomGameController::class, 'random'])->name('games.random');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+require __DIR__.'/auth.php';
